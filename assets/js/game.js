459440b80a497270f54c7349dcf61043698c794e
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2021-11-29 18:18:36 -0500

    more learning on variables, functions, and devtools

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,41 @@
-// this creates a function called "fight"
-
+// this is another window function called a prompt. allows user to input a name for their robot.
+// when we add var playerName = in front of the window.prompt function we use a variable to store the player's 
+// name in the browser's memory.
+var playerName = window.prompt("What is your robot's name?");
+// this adds an alert with the player's name:
+// window.alert(playerName);
+// this will show the player's name input by the user in the console:
+console.log(playerName); 
+console.log("This logs a string");
+// this sill do math and log 20
+console.log(10+10);
+// what is this? we use a + after the closing quotation, then write the variable name in order 
+// to combine a string with a variable - aka string concatenation:
+console.log("Our robot's name is " + playerName);
+// this creates a function called "fight" we are declaring a function
 function fight() {
     window.alert("The fight has begun!");
 }
+// we have to call the fight function declared above for the text in the alert window to show up
+
+// fight();
+
+
+
+
+
+
+
+
+
+
+
+// This is a String data type; it must be wrapped in double quotes (" ") or single quotes (' ').
+// var stringDataType = "This is a string, which is a fancy way to say text";
+
+// This is a Number data type; it can be an integer (whole number) or have decimals (floated numbers).
+// var numberIntegerDataType = 10;
+// var numberFloatDataType = 10.4;
+
+// This is a Boolean data type, which can only be given a value of true or false.
+// var booleanDataType = true;
