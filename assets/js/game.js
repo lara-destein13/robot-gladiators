8ddf89c81220b872bbb4fad4eb7304419cba704b
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2021-12-01 18:22:06 -0500

    more building structure of gameplay

--- assets/js/game.js
+++ assets/js/game.js
@@ -37,7 +37,8 @@ var enemyAttack = 12;
 var fight = function() {
     // Alert players that theyare starting the round
     window.alert("Welcome to Robot Gladiators!");
-
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    
     // Subtract the value of 'playerAttack from the value of 'enemyHealth' and use that result to
     // update the value in 'enemyHealth' variable
     enemyHealth = enemyHealth - playerAttack;
@@ -47,12 +48,47 @@ var fight = function() {
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
 
+    // if player chooses to fight, then fight
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+        // remove enemy's health by subtracting the amount set in the playerAttack variable
+        enemyHealth = enemyHealth - playerAttack;
+        console.log(
+            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + "health remaining."
+        );
+    }
+
     // check enemy's health
     if (enemyHealth <= 0) {
         window.alert(enemyName + " has died!");
-    }
-    else {
+    } else {
         window.alert(enemyName + " still has " + enemyHealth + "health left.");
+    }
+
+    // remove player's health by subtracting the amount set in the enemyAttack variable
+    playerHealth = playerHealth - enemyAttack;
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+
+
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(
+            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + "health remaining."
+        );
+    }
+
+
+    // remove enemy's health by subtracting the amount set in the playerAttack variable
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + "health remaining.");
+    }
+
+    // check player's health
+    if (playerHealth <=0) {
+            window.alert(playerName + "has died!");
+    } else {
+        window.alert(playerName + " has died!");
     }
 
     // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update 
