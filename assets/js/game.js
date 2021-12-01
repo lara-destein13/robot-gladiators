581cd4c73b63e9f31e6573b0f5e826f0866c6b82
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2021-12-01 17:16:47 -0500

    more game js

--- assets/js/game.js
+++ assets/js/game.js
@@ -44,15 +44,34 @@ var fight = function() {
 
     // Log a resulting message to the console so we know that it worked. 
     console.log(
-        playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining."
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
 
+    // check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + "health left.");
+    }
 
     // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update 
     // the value in the 'playerHealth' variable.
+    playerHealth = playerHealth - enemyAttack;
 
     // Log a resulting message to the console so we know that it worked
-    
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining. "
+    );    
+
+    // check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + "has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + "health left.");
+    }
+
 }
 
 fight();
@@ -76,6 +95,8 @@ fight();
 
 // This is a Boolean data type, which can only be given a value of true or false.
 // var booleanDataType = true;
+
+
 
 
 // write a function declaration for fight
