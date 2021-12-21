366e98f0624182e05ca5e5244a0a5af184d634cf
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2021-12-21 17:37:20 -0500

    more work on game

--- assets/js/game.js
+++ assets/js/game.js
@@ -41,48 +41,51 @@ var enemyAttack = 12;
 // This is a function expression: This is when we create a function by assigning it to a variable.
 // The following code shows an example. First we create the function, then we execture a function.
 var fight = function() {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+};
 
     // Alert players that theyare starting the round
-    window.alert("Welcome to Robot Gladiators!");
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    // window.alert("Welcome to Robot Gladiators!");
+    // var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     
-//     // Subtract the value of 'playerAttack from the value of 'enemyHealth' and use that result to
-//     // update the value in 'enemyHealth' variable
-//     enemyHealth = enemyHealth - playerAttack;
+//  // Subtract the value of 'playerAttack from the value of 'enemyHealth' and use that result to
+//  // update the value in 'enemyHealth' variable
+        enemyHealth = enemyHealth - playerAttack;
 
-//     // Log a resulting message to the console so we know that it worked. 
-//     console.log(
-//         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-//     );
+//  // Log a resulting message to the console so we know that it worked. 
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
 
-//     // if player chooses to fight, then fight
-//     if (promptFight === "fight" || promptFight === "FIGHT") {
-//         // remove enemy's health by subtracting the amount set in the playerAttack variable
-//         enemyHealth = enemyHealth - playerAttack;
-//         console.log(
-//             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + "health remaining."
-//         );
-//     }
+    // if player chooses to fight, then fight
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+        // remove enemy's health by subtracting the amount set in the playerAttack variable
+        enemyHealth = enemyHealth - playerAttack;
+        console.log(
+            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + "health remaining."
+        );
+    }
 
-//     // check enemy's health
-//     if (enemyHealth <= 0) {
-//         window.alert(enemyName + " has died!");
-//     } else {
-//         window.alert(enemyName + " still has " + enemyHealth + "health left.");
-//     }
+    // check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    } else {
+        window.alert(enemyName + " still has " + enemyHealth + "health left.");
+    }
 
-//     // remove player's health by subtracting the amount set in the enemyAttack variable
-//     playerHealth = playerHealth - enemyAttack;
-//     console.log(
-//         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-//     );
+    // remove player's health by subtracting the amount set in the enemyAttack variable
+    playerHealth = playerHealth - enemyAttack;
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
 
 
-//     enemyHealth = enemyHealth - playerAttack;
-//     console.log(
-//             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + "health remaining."
-//         );
-//     }
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(
+            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + "health remaining."
+    );
+    
 
 
 //     // remove enemy's health by subtracting the amount set in the playerAttack variable
@@ -102,20 +105,19 @@ var fight = function() {
 //     // the value in the 'playerHealth' variable.
 //     playerHealth = playerHealth - enemyAttack;
 
-//     // Log a resulting message to the console so we know that it worked
-//     console.log(
-//         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining. "
-//     );    
+    // Log a resulting message to the console so we know that it worked
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining. "
+    );    
 
-//     // check player's health
-//     if (playerHealth <= 0) {
-//         window.alert(playerName + "has died!");
-//     }
-//     else {
-//         window.alert(playerName + " still has " + playerHealth + "health left.");
-//     }
+    // check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + "has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + "health left.");
+    }
 
-}
 
 fight();
 
@@ -141,20 +143,19 @@ fight();
 
 
 
-// write a function declaration for fight
+// // create function (function declaration)
 // function fight() {
-//     window.alert("the fight has begun")
+//     window.alert("Welcome to Robot Gladiators!");
 // }
-
+// // execute function
 // fight();
 
-
-// write a function expression for fight
-
+// // create function (function expression)
 // var fight = function() {
-//     window.alert("the fight has begun");
-// }
-
+//     window.alert("Welcome to Robot Gladiators!");
+// };
+// // execute function
 // fight();
+
 
 
