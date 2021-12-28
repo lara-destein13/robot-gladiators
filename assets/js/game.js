4ded4c9fdae8b0cad3e736997227ae8ab8da68e6
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2021-12-28 16:52:37 -0500

    structure loops so that all three enemy robots fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,9 +19,8 @@ var enemyAttack = 12;
 // This is a function (our fight function) expression: This is when we create a function by assigning it to a variable.
 // The following code shows an example. First we create the function, then we execture a function.
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
-
+    // repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
     // ask player if they would like to fight or run
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -73,12 +72,16 @@ var fight = function(enemyName) {
         } else {
             window.alert("You need to pick a valid option. Try again!");
         }
-    };  
+    }    
+};  
 
-    // run the fight function to start game
-    for(var i = 0; i < enemyNames.length; i++) {
-        fight(enemyNames[i]);
-    }
+
+// run the fight function to start game
+for(var i = 0; i < enemyNames.length; i++) {
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(enemyNames[i]);
+}
 
 
     
